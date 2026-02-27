a5edfee4bb3be610b725f61b495860b42fcfa4ce
diogoaniceto <diogo.aniceto@vennx.com.br>
2026-02-26 22:59:38 -0300

    fix: change BrowserRouter basename for github pages

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/pe-de-serra">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/cafes" element={<CafesPage />} />
